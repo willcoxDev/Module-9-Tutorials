6a9cb7d69768f090b7b43e367c9ab51ee60e11af
Rick Willcox <rickwillcoxau@outlook.com>
2017-06-26 11:54:39 +1000

    started janes clothing

---
+++ JanesClothing/JanesClothing/CustomerAdd.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace JanesClothing
+{
+    partial class frmCustomerAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtSuberb = new System.Windows.Forms.TextBox();
+            this.txtPostcode = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.cbCategoryID = new System.Windows.Forms.ComboBox();
+            this.cbState = new System.Windows.Forms.ComboBox();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.chkYes = new System.Windows.Forms.CheckBox();
+            this.chkNo = new System.Windows.Forms.CheckBox();
+            this.gbCustomerDetails = new System.Windows.Forms.GroupBox();
+            this.bgCustomerAddress = new System.Windows.Forms.GroupBox();
+            this.gbOfficeUseOnly = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.gbCustomerDetails.SuspendLayout();
+            this.bgCustomerAddress.SuspendLayout();
+            this.gbOfficeUseOnly.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Postcode";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "State";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(220, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Send Catalogue";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Gender";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 32);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Suberb";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 59);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(119, 32);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Address";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 108);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(171, 32);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Customer ID";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(17, 198);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(130, 32);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Category";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(224, 37);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(237, 38);
+            this.txtFirstName.TabIndex = 10;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(224, 53);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(237, 38);
+            this.txtAddress.TabIndex = 11;
+            // 
+            // txtSuberb
+            // 
+            this.txtSuberb.Location = new System.Drawing.Point(224, 107);
+            this.txtSuberb.Name = "txtSuberb";
+            this.txtSuberb.Size = new System.Drawing.Size(237, 38);
+            this.txtSuberb.TabIndex = 12;
+            // 
+            // txtPostcode
+            // 
+            this.txtPostcode.Location = new System.Drawing.Point(224, 212);
+            this.txtPostcode.Name = "txtPostcode";
+            this.txtPostcode.Size = new System.Drawing.Size(237, 38);
+            this.txtPostcode.TabIndex = 14;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(224, 85);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(237, 38);
+            this.txtLastName.TabIndex = 15;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(232, 105);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(237, 38);
+            this.txtCustomerID.TabIndex = 16;
+            // 
+            // cbCategoryID
+            // 
+            this.cbCategoryID.FormattingEnabled = true;
+            this.cbCategoryID.Location = new System.Drawing.Point(224, 191);
+            this.cbCategoryID.Name = "cbCategoryID";
+            this.cbCategoryID.Size = new System.Drawing.Size(237, 39);
+            this.cbCategoryID.TabIndex = 17;
+            // 
+            // cbState
+            // 
+            this.cbState.FormattingEnabled = true;
+            this.cbState.Location = new System.Drawing.Point(224, 160);
+            this.cbState.Name = "cbState";
+            this.cbState.Size = new System.Drawing.Size(237, 39);
+            this.cbState.TabIndex = 18;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(224, 139);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(114, 36);
+            this.rbMale.TabIndex = 19;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(366, 139);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(147, 36);
+            this.rbFemale.TabIndex = 20;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // chkYes
+            // 
+            this.chkYes.AutoSize = true;
+            this.chkYes.Location = new System.Drawing.Point(263, 50);
+            this.chkYes.Name = "chkYes";
+            this.chkYes.Size = new System.Drawing.Size(102, 36);
+            this.chkYes.TabIndex = 21;
+            this.chkYes.Text = "Yes";
+            this.chkYes.UseVisualStyleBackColor = true;
+            // 
+            // chkNo
+            // 
+            this.chkNo.AutoSize = true;
+            this.chkNo.Location = new System.Drawing.Point(371, 50);
+            this.chkNo.Name = "chkNo";
+            this.chkNo.Size = new System.Drawing.Size(89, 36);
+            this.chkNo.TabIndex = 22;
+            this.chkNo.Text = "No";
+            this.chkNo.UseVisualStyleBackColor = true;
+            // 
+            // gbCustomerDetails
+            // 
+            this.gbCustomerDetails.Controls.Add(this.label1);
+            this.gbCustomerDetails.Controls.Add(this.txtFirstName);
+            this.gbCustomerDetails.Controls.Add(this.label2);
+            this.gbCustomerDetails.Controls.Add(this.rbFemale);
+            this.gbCustomerDetails.Controls.Add(this.cbCategoryID);
+            this.gbCustomerDetails.Controls.Add(this.txtLastName);
+            this.gbCustomerDetails.Controls.Add(this.rbMale);
+            this.gbCustomerDetails.Controls.Add(this.label6);
+            this.gbCustomerDetails.Controls.Add(this.label10);
+            this.gbCustomerDetails.Location = new System.Drawing.Point(114, 191);
+            this.gbCustomerDetails.Name = "gbCustomerDetails";
+            this.gbCustomerDetails.Size = new System.Drawing.Size(529, 291);
+            this.gbCustomerDetails.TabIndex = 23;
+            this.gbCustomerDetails.TabStop = false;
+            this.gbCustomerDetails.Text = "Customer Details";
+            // 
+            // bgCustomerAddress
+            // 
+            this.bgCustomerAddress.Controls.Add(this.txtSuberb);
+            this.bgCustomerAddress.Controls.Add(this.label7);
+            this.bgCustomerAddress.Controls.Add(this.label8);
+            this.bgCustomerAddress.Controls.Add(this.txtAddress);
+            this.bgCustomerAddress.Controls.Add(this.cbState);
+            this.bgCustomerAddress.Controls.Add(this.label4);
+            this.bgCustomerAddress.Controls.Add(this.label3);
+            this.bgCustomerAddress.Controls.Add(this.txtPostcode);
+            this.bgCustomerAddress.Location = new System.Drawing.Point(114, 581);
+            this.bgCustomerAddress.Name = "bgCustomerAddress";
+            this.bgCustomerAddress.Size = new System.Drawing.Size(529, 286);
+            this.bgCustomerAddress.TabIndex = 24;
+            this.bgCustomerAddress.TabStop = false;
+            this.bgCustomerAddress.Text = "Customer Address";
+            // 
+            // gbOfficeUseOnly
+            // 
+            this.gbOfficeUseOnly.Controls.Add(this.label5);
+            this.gbOfficeUseOnly.Controls.Add(this.label9);
+            this.gbOfficeUseOnly.Controls.Add(this.txtCustomerID);
+            this.gbOfficeUseOnly.Controls.Add(this.chkNo);
+            this.gbOfficeUseOnly.Controls.Add(this.chkYes);
+            this.gbOfficeUseOnly.Location = new System.Drawing.Point(790, 191);
+            this.gbOfficeUseOnly.Name = "gbOfficeUseOnly";
+            this.gbOfficeUseOnly.Size = new System.Drawing.Size(529, 171);
+            this.gbOfficeUseOnly.TabIndex = 25;
+            this.gbOfficeUseOnly.TabStop = false;
+            this.gbOfficeUseOnly.Text = "Office Use Only";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(1093, 648);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(166, 46);
+            this.btnAdd.TabIndex = 26;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(1093, 715);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(166, 46);
+            this.BtnCancel.TabIndex = 27;
+            this.BtnCancel.Text = "&Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmCustomerAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 994);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.gbOfficeUseOnly);
+            this.Controls.Add(this.bgCustomerAddress);
+            this.Controls.Add(this.gbCustomerDetails);
+            this.Name = "frmCustomerAdd";
+            this.Text = "Add Customer Details";
+            this.gbCustomerDetails.ResumeLayout(false);
+            this.gbCustomerDetails.PerformLayout();
+            this.bgCustomerAddress.ResumeLayout(false);
+            this.bgCustomerAddress.PerformLayout();
+            this.gbOfficeUseOnly.ResumeLayout(false);
+            this.gbOfficeUseOnly.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtSuberb;
+        private System.Windows.Forms.TextBox txtPostcode;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.ComboBox cbCategoryID;
+        private System.Windows.Forms.ComboBox cbState;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.CheckBox chkYes;
+        private System.Windows.Forms.CheckBox chkNo;
+        private System.Windows.Forms.GroupBox gbCustomerDetails;
+        private System.Windows.Forms.GroupBox bgCustomerAddress;
+        private System.Windows.Forms.GroupBox gbOfficeUseOnly;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button BtnCancel;
+    }
+}
+
--- JanesClothing/JanesClothing/CustomerAdd.cs
+++ JanesClothing/JanesClothing/CustomerAdd.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace JanesClothing
 {
-    public partial class Form1 : Form
+    public partial class frmCustomerAdd : Form
     {
-        public Form1()
+        public frmCustomerAdd()
         {
             InitializeComponent();
         }
